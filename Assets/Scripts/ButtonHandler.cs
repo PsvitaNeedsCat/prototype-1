35da683a189d9b4bb91f2a6fc68df1b46d5aa224
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-02-11 12:18:18 +1300

    TextMeshWIP

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    PlayersJoined numPlayers;
+
+    [SerializeField] GameObject[] playerButtons;
+    [SerializeField] GameObject[] buttonText;
+
+    private void Awake()
+    {
+        numPlayers = GameObject.Find("PlayersJoined").GetComponent<PlayersJoined>();
+    }
+
+    public void Play()
+    {
+        // Load game scene
+        SceneManager.LoadScene("CarTest");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void AddPlayer()
+    {
+        uint playersJoined = numPlayers.playerCount;
+
+        switch (playersJoined)
+        {
+            case 0:
+                {
+                    // Display player 2 button, then add to player count
+                    if (playerButtons[1])
+                    {
+                        playerButtons[1].SetActive(true);
+                    }
+
+                    buttonText[0].GetComponent<TextMesh>().text = "Joined";
+
+                    break;
+                }
+
+            case 1:
+                {
+                    // Add a player
+                    break;
+                }
+
+            case 2:
+                {
+                    // Max number of players, do nothing
+                    return;
+                }
+
+            default:
+                break;
+        }
+
+        numPlayers.playerCount++;
+    }
+}
---
+++ Assets/Scripts/PlayersJoined.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersJoined : MonoBehaviour
+{
+    public uint playerCount = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
