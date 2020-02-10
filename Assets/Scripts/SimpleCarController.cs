6affc6b18c9ccc4b6138d2372fa3885021586b5b
Tim Sinclair <timbbby@gmail.com>
2020-02-10 21:35:23 +1300

    Drivable car

---
+++ Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private float steeringAngle;
+
+    public WheelCollider frontLeftWheel, frontRightWheel, backLeftWheel, backRightWheel;
+    public Transform frontLeftTransform, frontRightTransform, backLeftTransform, backRightTransform;
+    public float maxSteerAngle = 30;
+    public float motorForce = 50;
+
+    public void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        steeringAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheel.steerAngle = steeringAngle;
+        frontRightWheel.steerAngle = steeringAngle;
+    }
+
+    private void Accelerate()
+    {
+        frontLeftWheel.motorTorque = verticalInput * motorForce;
+        frontRightWheel.motorTorque = verticalInput * motorForce;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontLeftWheel, frontLeftTransform);
+        UpdateWheelPose(frontRightWheel, frontRightTransform);
+        UpdateWheelPose(backLeftWheel, backLeftTransform);
+        UpdateWheelPose(backRightWheel, backRightTransform);
+    }
+
+    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 pos = _transform.position;
+        Quaternion quat = _transform.rotation;
+
+        _collider.GetWorldPose(out pos, out quat);
+
+        _transform.position = pos;
+        _transform.rotation = quat;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+}
