d687a4df64571aada679c03d153ad9410d578109
Callum <53197859+PsvitaNeedsCat@users.noreply.github.com>
2020-02-14 09:45:35 +1300

    Horn Fix

--- Assets/Scripts/HornScript.cs
+++ Assets/Scripts/HornScript.cs
@@ -4,38 +4,52 @@ using UnityEngine;
 
 public class HornScript : MonoBehaviour
 {
-    public AudioSource[] hornSounds;
-
+    public AudioClip[] hornSounds;
+    AudioSource m_audioSource;
 
     public enum Sounds
     { 
-        SQUEAK = 0
+        SQUEAK = 0,
+        CLASSIC
     }
 
     public Sounds hornIndex = Sounds.SQUEAK;
 
+    private void Awake()
+    {
+        // Set audio source
+        m_audioSource = GetComponent<AudioSource>();
+
+        // Set audio clip
+        m_audioSource.clip = hornSounds[(int)hornIndex];
+    }
+
     public void PlayHorn()
     {
-        if (!hornSounds[(int)hornIndex].isPlaying)
-            hornSounds[(int)hornIndex].Play();
+        if (!m_audioSource.isPlaying)
+            m_audioSource.Play();
     }
 
     public void StopHorn()
     {
-        if (hornSounds[(int)hornIndex].isPlaying)
-            hornSounds[(int)hornIndex].Stop();
+        if (m_audioSource.isPlaying)
+            m_audioSource.Stop();
     }
 
     public void ChangeHorn(Sounds _newSound)
     {
-        int oldSound = (int)hornIndex;
         hornIndex = _newSound;
 
         // If horn is playing, swap sound
-        if (hornSounds[oldSound].isPlaying)
+        if (m_audioSource.isPlaying)
         {
-            hornSounds[oldSound].Stop();
-            hornSounds[(int)hornIndex].Play();
+            m_audioSource.Stop();
+            m_audioSource.clip = hornSounds[(int)_newSound];
+            m_audioSource.Play();
+        }
+        else
+        {
+            m_audioSource.clip = hornSounds[(int)_newSound];
         }
     }
 }
