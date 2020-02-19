cf5dcea91b3194b578b0940335f77acee4af4e3a
Tim <timbbby@gmail.com>
2020-02-20 12:02:02 +1300

    More sounds

---
+++ Assets/Scripts/MusicFadeIn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicFadeIn : MonoBehaviour
+{
+    private AudioSource musicSource;
+    public float overSeconds = 2.0f;
+    private void Awake()
+    {
+        musicSource = GetComponent<AudioSource>();
+        
+    }
+
+    private void Update()
+    {
+        if (musicSource.volume < 1.0f) { musicSource.volume += Time.deltaTime / overSeconds; }
+    }
+}
